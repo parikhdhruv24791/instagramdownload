8bde6a5c15a639d23d447270b11971aa38f5f337
Dhruv Parikh <parikhdhruv24791@gmail.com>
2017-05-19 11:08:01 +0530

    Added icons and removed server

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, { file: "lib/jquery.min.js" }, function() {
         chrome.tabs.executeScript(null, {file: "src/inject.js"});
     });
-   
+   console.log("Icons made by http://www.flaticon.com/authors/hadrien (Hadrien) from http://www.flaticon.com is licensed by Creative Commons BY 3.0 BY");
 });
